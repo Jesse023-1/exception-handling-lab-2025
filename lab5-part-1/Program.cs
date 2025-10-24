2269ed7759201d45ebff2feade9c505c097f1807
Jesse <jehno_dizon@yahoo.ca>
2025-10-24 13:39:47 -0400

    Exception Handling

--- lab5-part-1/Program.cs
+++ lab5-part-1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,34 @@ namespace lab5_part_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first number:");
-            int first = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Enter first number:");
+                string first = Console.ReadLine();
+                int number1 = int.Parse(first);
+                Console.WriteLine("Enter second number:");
+                string second = Console.ReadLine();
+                int number2 = int.Parse(second);
+                int answer = number1 / number2;
+                Console.WriteLine(answer);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                Console.WriteLine("Cannot be divided by 0");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Cannot be empty or a string");
+            }
+            finally
+            {
+             using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry("Log message example", EventLogEntryType.Information, 101, 1);
+                }   
+            }
         }
     }
 }

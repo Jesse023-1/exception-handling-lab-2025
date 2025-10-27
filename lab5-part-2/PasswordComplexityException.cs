981e92076a80f0b2f1161505548c7fdb14c4e526
Jesse <jehno_dizon@yahoo.ca>
2025-10-27 10:18:39 -0400

    Finished lab5-part-2

---
+++ lab5-part-2/PasswordComplexityException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab5_part_2
+{
+    internal class PasswordComplexityException : Exception
+    {
+        private string message { get; set; }
+
+        public PasswordComplexityException(string message)
+        {
+            this.message = message;
+        }
+
+        public override string Message
+        {
+            get { return string.Format("Password creation error: {0}", message); }
+        }
+    }
+}
---
+++ lab5-part-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Create a password:");
+                string Pw = Console.ReadLine();
+
+                if (Pw.Length < 8)
+                {
+                    PasswordComplexityException ex = new PasswordComplexityException("Password below minimum 8 characters");
+                    throw ex;
+                }
+            }
+            catch (PasswordComplexityException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

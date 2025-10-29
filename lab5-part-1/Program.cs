be7aa4a9b73e55cd748e1183431e97044edf56ea
Jesse <jehno_dizon@yahoo.ca>
2025-10-29 17:53:19 -0400

    Redid EventLog and changed pictures

--- lab5-part-1/Program.cs
+++ lab5-part-1/Program.cs
@@ -11,33 +11,52 @@ namespace lab5_part_1
     {
         static void Main(string[] args)
         {
-            try
+            using (EventLog eventlog = new EventLog())
             {
-                Console.WriteLine("Enter first number:");
-                string first = Console.ReadLine();
-                int number1 = int.Parse(first);
-                Console.WriteLine("Enter second number:");
-                string second = Console.ReadLine();
-                int number2 = int.Parse(second);
-                int answer = number1 / number2;
-                Console.WriteLine(answer);
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("Cannot divide by 0");
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine("Cannot be empty or a string");
-            }
-            finally
-            {
-             using (EventLog eventLog = new EventLog("Application"))
+                string source = "Lab 5";
+                string logName = "Application";
+                try
                 {
-                    eventLog.Source = "Application";
-                    eventLog.WriteEntry("Log message example", EventLogEntryType.Information, 101, 1);
-                }   
+
+                    Console.WriteLine("Enter first number:");
+                    string first = Console.ReadLine();
+                    int number1 = int.Parse(first);
+                    Console.WriteLine("Enter second number:");
+                    string second = Console.ReadLine();
+                    int number2 = int.Parse(second);
+                    int answer = number1 / number2;
+                    Console.WriteLine(answer);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Cannot divide by 0");
+                    //string source = "Lab 5";
+                    //string logName = "Application";
+                    if (!EventLog.SourceExists(source))
+                        EventLog.CreateEventSource(source, logName);
+                    EventLog.WriteEntry(source, "Divided by Zero Exception", EventLogEntryType.Information, 101, 1);
+                    return;
+
+                }
+                catch (FormatException ex)
+                {
+                    ///string source = "Lab 5";
+                    //string logName = "Application";
+                    Console.WriteLine("Cannot be empty or a string");
+                    if (!EventLog.SourceExists(source))
+                        EventLog.CreateEventSource(source, logName);
+                    EventLog.WriteEntry(source, "Format Exception", EventLogEntryType.Information, 101, 2);
+                    return;
+                }
+                finally
+                {
+                    if (!EventLog.SourceExists(source))
+                        EventLog.CreateEventSource(source, logName);
+                    EventLog.WriteEntry(source, "Log message example", EventLogEntryType.Information, 101, 3);
+                    
+                }
             }
         }
+        
     }
 }
